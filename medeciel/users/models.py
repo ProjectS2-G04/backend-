e8848c79dab3385eaab7de23eb29b52004eda738
nessrine <n.mehda@esi-sba.dz>
2025-03-12 19:44:07 +0100

    Initial commit

---
+++ medeciel/users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+class User(AbstractUser):
+    username = None
+    full_name= models.CharField(max_length=150)
+    email =models.EmailField(_('email address'), unique= True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ([])
+    def __str__(self):
+        return self.full_name
+    
+
+
+
+    
