b116556f16c6882f0d657152d8e433b444d5e8ac
nessrine <n.mehda@esi-sba.dz>
2025-03-15 15:31:26 +0100

    signup registration

--- medeciel/users/views.py
+++ medeciel/users/views.py
@@ -22,10 +22,10 @@ class LogoutView(APIView):
             token = RefreshToken(refresh_token)
             token.blacklist()  
 
-            return Response({"message": "Successfully logged out"}, status=status.HTTP_205_RESET_CONTENT)
+            return Response({"message": "Successfully logged out "}, status=status.HTTP_205_RESET_CONTENT)
 
         except Exception as e:
-            return Response({"error": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "Invalid token !"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
